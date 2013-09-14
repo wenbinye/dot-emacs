11df55f609416fecc670c8faed6f622a56b845f0
Ye Wenbin <wenbinye@ookong.com>
2013-09-14 15:27:41 +0800

    phpunit support namespace

--- site-lisp/contrib/phpunit.el
+++ site-lisp/contrib/phpunit.el
@@ -26,6 +26,26 @@
 ;; Features privodes:
 ;;  1. automatic generate test
 ;;  2. run test for select function
+;;
+;; Add project config .phpunit-config
+;;   (setq phpunit-project-config
+;;       (append '((source-directory . "src/phplib")
+;;                 (get-test-namespace-function . phpunit-get-test-namespace-tests)
+;;                 (get-source-namespace-function . phpunit-get-source-namespace-tests))
+;;               phpunit-project-config))
+;;
+;; test class template example:
+;;    <?php
+;;    namespace (>>>test-namespace<<<);
+;;    
+;;    use (>>>namespace<<<)\\(>>>source-class<<<);
+;;    /**
+;;     * TestCase for (>>>source-class<<<)
+;;     */
+;;    class (>>>test-class<<<) extends \PHPUnit_Framework_TestCase
+;;    {
+;;    }
+;; 
 
 ;; Put this file into your load-path and the following into your ~/.emacs:
 ;;   (require 'phpunit)
@@ -35,6 +55,11 @@
 
 (eval-when-compile
   (require 'cl))
+(require 'template-simple)
+
+(defvar phpunit-namespace-regexp
+  "^\\s-*namespace\\s-+\\([a-zA-Z0-9\\]+\\)\\s-*"
+  "Regexp that match class declaration")
 
 (defvar phpunit-class-regexp
   "^\\s-*\\(?:abstract\\s-+\\)?class\\s-+\\(\\(?:\\sw\\|\\s_\\)+\\)\\s-*"
@@ -46,11 +71,13 @@
 
 (defvar phpunit-project-config
   '(
-    (get-test-class-function   . phpunit-get-test-class-suffix)
-    (get-source-class-function . phpunit-get-source-class-suffix)
-    (get-source-file-function  . phpunit-get-source-file-annotated)
-    (get-test-file-function    . phpunit-get-test-file-simple)
-    (get-file-type-function    . phpunit-get-file-type-simple)
+    (get-source-class-function     . phpunit-get-source-class-suffix)
+    (get-source-namespace-function . identity)
+    (get-source-file-function      . phpunit-get-source-file-simple)
+    (get-test-class-function       . phpunit-get-test-class-suffix)
+    (get-test-file-function        . phpunit-get-test-file-simple)
+    (get-test-namespace-function   . identity)
+    (get-file-type-function        . phpunit-get-file-type-simple)
     (source-directory      . "lib")
     (test-directory        . "tests")
     (source-file-extension . ".php")
@@ -94,13 +121,22 @@
     (if (re-search-backward phpunit-function-regexp nil t)
         (cons (match-string-no-properties 1) (match-string-no-properties 2)))))
 
+(defun phpunit-file-namespace ()
+  "Get current namespace"
+  (save-excursion
+    (goto-char (point-min))
+    (if (re-search-forward phpunit-namespace-regexp nil t)
+        (match-string-no-properties 1))))
+
 (defun phpunit-file-class ()
   "Get class name for current file"
   (or (phpunit-class-ap)
       (save-excursion
         (goto-char (point-min))
         (if (re-search-forward phpunit-class-regexp nil t)
-            (match-string-no-properties 1)))))
+            (match-string-no-properties 1)))
+      (let ((ext (phpunit-project-config 'source-file-extension)))
+        (file-name-nondirectory (substring buffer-file-name 0 (* -1 (length ext)))))))
 
 (defun phpunit-file-functions ()
   "Get all functions for current file"
@@ -130,9 +166,17 @@
   "Get test class name for `class'"
   (funcall (phpunit-project-config 'get-test-class-function) class))
 
+(defun phpunit-get-test-namespace (namespace)
+  "Get source namespace for test class namespace"
+  (funcall (phpunit-project-config 'get-test-namespace-function) namespace))
+
 (defun phpunit-get-source-class (test-class)
   "Get source class name for `test-class'"
   (funcall (phpunit-project-config 'get-source-class-function) test-class))
+
+(defun phpunit-get-source-namespace (test-namespace)
+  "Get source namespace for test class namespace"
+  (funcall (phpunit-project-config 'get-source-namespace-function) test-namespace))
 
 (defun phpunit-find-top-directory (file &optional dir)
   "Find `file' in all parent directories of `dir'(include `dir')"
@@ -152,29 +196,42 @@
 ;;       (let ((dir (read-directory-name "Project root directory: ")))
 ;;         (phpunit-build-cache-file dir)))))
 
-(defun phpunit-get-source-file (class)
+(defun phpunit-get-source-file (namespace class)
   "Get source file for `class'"
-  (funcall (phpunit-project-config 'get-source-file-function) class))
+  (funcall (phpunit-project-config 'get-source-file-function) namespace class))
 
-(defun phpunit-get-test-file (test-class)
+(defun phpunit-get-test-file (namespace test-class)
   "Get test file for `test-class'"
-  (funcall (phpunit-project-config 'get-test-file-function) test-class))
+  (funcall (phpunit-project-config 'get-test-file-function) namespace test-class))
 
-(defun phpunit-get-test-file-simple (test-class)
+(defun phpunit-get-test-file-simple (namespace class)
   "Get file in `test-directory'.
 The file name equals to test class name + `test-file-extension'."
   (let ((dir (phpunit-find-top-directory (phpunit-project-config 'test-directory))))
     (if dir
-        (concat (file-name-as-directory dir) test-class
+        (concat (file-name-as-directory dir)
+                (when namespace
+                    (concat (replace-regexp-in-string "\\\\" "/" namespace) "/"))
+                class
                 (phpunit-project-config 'test-file-extension))
       (message "Can't not find test directory '%s'" (phpunit-project-config 'test-directory)))))
 
-(defun phpunit-get-source-file-annotated (class)
+(defun phpunit-get-source-file-annotated (namespace class)
   "Find source file from annotation in current buffer when edit test file"
   (save-excursion
     (goto-char (point-min))
     (if (re-search-forward "@source\\s-+\\([--:\\\\$+<>@-Z_a-z~*?\x100-\xffff]+\\)" nil t)
         (match-string 1))))
+
+(defun phpunit-get-source-file-simple (namespace class)
+  (let ((dir (phpunit-find-top-directory (phpunit-project-config 'source-directory))))
+    (if dir
+        (concat (file-name-as-directory dir)
+                (when namespace
+                    (concat (replace-regexp-in-string "\\\\" "/" namespace) "/"))
+                class
+                (phpunit-project-config 'source-file-extension))
+      (message "Can't not find source directory '%s'" (phpunit-project-config 'source-directory)))))
 
 (defun phpunit-get-test-file-pear (test-class)
   "Find test file using pear class name rule"
@@ -194,13 +251,13 @@ The file name equals to test class name + `test-file-extension'."
                 (phpunit-project-config 'source-file-extension))
       (message "Can't not find test directory '%s'" (phpunit-project-config 'source-directory)))))
 
-(defun phpunit-get-file-type ()
+(defun phpunit-get-file-type (class)
   "Detect file whether is test class or source class"
-  (funcall (phpunit-project-config 'get-file-type-function)))
+  (funcall (phpunit-project-config 'get-file-type-function) class))
 
-(defun phpunit-get-file-type-simple ()
+(defun phpunit-get-file-type-simple (class)
   "Detect php file type by check whether current class name end with 'Test'"
-  (if (string-match "Test$" (phpunit-file-class))
+  (if (string-match "Test$" class)
       'test
     'source))
 
@@ -213,30 +270,44 @@ The file name equals to test class name + `test-file-extension'."
       (when file
         (load file t)))))
 
-(defun phpunit-create-test-template (test-class test-file source-class source-file)
+(defun phpunit-create-test-template (test-namespace test-class test-file namespace source-class source-file)
   "create test code using `template-simple'"
   (let ((file (locate-file phpunit-template-file template-directory-list)))
     (if file
         (template-simple-expand-template file)
       (message "Template file '%s' is not exists" phpunit-template-file))))
 
-(defun phpunit-create-test-simple (test-class test-file source-class source-file)
+(defun phpunit-create-test-simple (test-namespace test-class test-file namespace source-class source-file)
   "Create test code"
-  (insert (format "<?php
-/**
+  (let ((parent-class "PHPUnit_Framework_TestCase"))
+    (insert "<?php\n"
+            (when test-namespace
+              (setq parent-class (concat "\\" parent-class))
+              (concat "namespace " test-namespace ";\n\n"
+                      "use " namespace "\\" source-class ";\n\n"))
+            (format "/**
  * TestCase for %s
  * @source %s
  */
-require_once('PHPUnit/Framework.php');
-require_once('%s');
-class %s extends PHPUnit_Framework_TestCase
+class %s extends %s
 {
     protected function setUp()
     {
     }
 }
 "
-                  source-class source-file source-file test-class)))
+                    source-class source-file test-class parent-class))))
+
+(defun phpunit-get-test-namespace-tests (namespace)
+  "create test namespace for Php\\Class as Php\\Tests\\Class"
+  (when namespace
+    (let ((path (split-string namespace "\\\\")))
+      (mapconcat 'identity (append (list (car path) "Tests") (cdr path)) "\\"))))
+
+(defun phpunit-get-source-namespace-tests (test-namespace)
+  "remove \\Tests in test-namespace"
+  (when namespace
+    (replace-regexp-in-string "\\\\Tests\\\\" "\\" test-namespace)))
 
 (defun phpunit-create-test-1 (test-class test-file source-class source-file &optional other-window)
   (funcall (if other-window 'find-file-other-window 'find-file) test-file)
@@ -275,90 +346,94 @@ class %s extends PHPUnit_Framework_TestCase
   (phpunit-load-config)
   (let ((function (phpunit-function-ap))
         (class (phpunit-file-class))
+        (namespace (phpunit-file-namespace))
         (find-file-function (if other-window 'find-file-other-window 'find-file))
-        file)
+        test-file source-file)
     (if class
-        (if (eq (phpunit-get-file-type) 'test)
+        (if (eq (phpunit-get-file-type class) 'test)
             (progn
-              (setq file (phpunit-get-source-file (phpunit-get-source-class class)))
-              (if (and file (file-exists-p file))
+              (setq source-file (phpunit-get-source-file (phpunit-get-source-namespace namespace)
+                                                         (phpunit-get-source-class class)))
+              (if (and source-file (file-exists-p source-file))
                   (progn
-                    (funcall find-file-function file)
+                    (funcall find-file-function source-file)
                     (and function
                          (phpunit-goto-function (phpunit-get-source-function (cdr function)))))
                 (message "Can't locate source file for class '%s'" class)))
-          (let ((test-class (phpunit-get-test-class class)))
-            (setq file (phpunit-get-test-file test-class))
-            (if (and file (file-exists-p file))
+          (let ((test-namespace (phpunit-get-test-namespace namespace))
+                (test-class (phpunit-get-test-class class)))
+            (setq test-file (phpunit-get-test-file test-namespace test-class)
+                  source-file (file-relative-name buffer-file-name (file-name-directory test-file)))
+            (if (and test-file (file-exists-p test-file))
                 (progn
-                  (funcall find-file-function file)
+                  (funcall find-file-function test-file)
                   (and function
                        (phpunit-goto-function (phpunit-get-test-function (cdr function)))))
-              (when (y-or-n-p (format "Test class '%s' not exists, create file %s" test-class file))
-                (when (not (file-exists-p (file-name-directory file)))
-                  (make-directory (file-name-directory file) t))
-                (phpunit-create-test-1 test-class file
-                                          class (file-relative-name buffer-file-name (file-name-directory file))
-                                          other-window)))))
+              (when (y-or-n-p (format "Test class '%s' not exists, create file %s" test-class test-file))
+                (when (not (file-exists-p (file-name-directory test-file)))
+                  (make-directory (file-name-directory test-file) t))
+                (funcall find-file-function test-file)
+                (funcall phpunit-create-test-function test-namespace test-class test-file namespace class source-file)
+                (save-buffer)))))
       (message "No class found in current buffer"))))
 
 (defun phpunit-create-test (&optional all)
   "Create test method for current class.
 With prefix argument, create all test function in current class"
   (interactive "P")
-  (if (eq (phpunit-get-file-type) 'source)
-      (let ((class (phpunit-file-class))
-            (source-file buffer-file-name)
-            functions class-end pos)
-        (dolist (function
-                 (if all
-                     (phpunit-file-functions)
-                   (list (phpunit-function-ap))))
-          (when (and (consp function)
-                     (not (string-match "protected\\|private" (car function))))
-            (push (phpunit-get-test-function (cdr function)) functions)))
-        (phpunit-switch)
-        (if functions
-            (save-excursion
-              (goto-char (point-min))
-              (when (not (re-search-forward phpunit-class-regexp nil t))
-                (save-excursion 
-                  (funcall phpunit-create-test-function
-                           (phpunit-get-test-class class) buffer-file-name
-                           class source-file))
-                (re-search-forward phpunit-class-regexp nil t))
-              (when (re-search-forward "{" nil t)
-                (backward-char 1)
-                (setq class-end (copy-marker (scan-sexps (point) 1)))
-                (dolist (function functions)
-                  (unless (phpunit-function-position function)
-                    (goto-char class-end)
-                    (backward-char 1)
-                    (delete-region (progn (skip-chars-backward " \n\t") (point)) (1- class-end))
-                    (insert (format "\n\n    function %s()\n    {\n        \n    }\n" function))
-                    (setq pos (- (point) 7))))))
-          (message "No function to create test"))
-        (if pos (goto-char pos)))
-    (message "Not in source file")))
+  (let ((class (phpunit-file-class)))
+    (if (and class (eq (phpunit-get-file-type class) 'source))
+        (let ((namespace (phpunit-file-namespace))
+              (source-file buffer-file-name)
+              functions class-end pos)
+          (dolist (function
+                   (if all
+                       (phpunit-file-functions)
+                     (list (phpunit-function-ap))))
+            (when (and (consp function)
+                       (not (string-match "protected\\|private" (car function))))
+              (push (phpunit-get-test-function (cdr function)) functions)))
+          (phpunit-switch)
+          (if functions
+              (save-excursion
+                (goto-char (point-min))
+                (when (not (re-search-forward phpunit-class-regexp nil t))
+                  (save-excursion 
+                    (funcall phpunit-create-test-function
+                             (phpunit-get-test-namespace namespace) (phpunit-get-test-class class) buffer-file-name
+                             namespace class source-file))
+                  (re-search-forward phpunit-class-regexp nil t))
+                (when (re-search-forward "{" nil t)
+                  (backward-char 1)
+                  (setq class-end (copy-marker (scan-sexps (point) 1)))
+                  (dolist (function functions)
+                    (unless (phpunit-function-position function)
+                      (goto-char class-end)
+                      (backward-char 1)
+                      (delete-region (progn (skip-chars-backward " \n\t") (point)) (1- class-end))
+                      (insert (format "\n\n    function %s()\n    {\n        \n    }\n" function))
+                      (setq pos (- (point) 7))))))
+            (message "No function to create test"))
+          (if pos (goto-char pos)))
+      (message "Not in source file"))))
 
 (defun phpunit-run-test (&optional all)
   "Run test for current function."
   (interactive "P")
   (let ((class (phpunit-file-class))
-        (function (phpunit-function-ap))
-        (file-type (phpunit-get-file-type))
-        test-class test-file test-function compile-command)
+        function file-type test-file test-function compile-command)
     (if class
         (progn
-          (if (eq file-type 'source)
-              (setq test-file (phpunit-get-test-file (phpunit-get-test-class class))
-                    test-class (phpunit-get-test-class class))
-            (setq test-file buffer-file-name
-                  test-class class))
+          (setq function (phpunit-function-ap)
+                file-type (phpunit-get-file-type class)
+                test-file (if (eq file-type 'source)
+                              (phpunit-get-test-file (phpunit-get-test-class class))
+                            buffer-file-name))
           (if all
-              (setq compile-command (format "phpunit %s \"%s\"" class test-file))
+              (setq compile-command (format "phpunit \"%s\"" test-file))
             (setq test-function (if (eq file-type 'source) (phpunit-get-test-function (cdr function)) (cdr function)))
-            (setq compile-command (format "phpunit --filter /::%s$/ %s \"%s\"" test-function class test-file)))
+            (setq compile-command (format "phpunit --filter /::%s$/ \"%s\"" test-function test-file)))
+          (setq compilation-directory (phpunit-find-top-directory (phpunit-project-config 'test-directory)))
           (call-interactively 'compile))
       (message "No test found"))))
 
